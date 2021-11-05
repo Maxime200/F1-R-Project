58a1b37031a9354355442c09a39790b20aa7d2d7
Maxime200 <maime.bourgain@edu.esiee.fr>
2021-11-05 14:56:34 +0100

    final

--- F1_dataviz_r.r
+++ F1_dataviz_r.r
@@ -1,8 +1,14 @@
-if (require("tidyverse")) {
-  library(tidyverse)
+###########################################################################################################################
+#
+# Partie I : Instalation des packages et téléchargement du dataset
+#
+###########################################################################################################################
+
+if (require("tidyverse")) {     # Vérification si le package est déjà instalé 
+  library(tidyverse)            # Si tel est le cas nous le chargeons
 } else {
-  install.packages("tidyverse")
-  library(tidyverse)
+  install.packages("tidyverse") # Sinon nous le téléchargeons
+  library(tidyverse)            # Puis nous le chargeons
 }
 
 if (require("plotly")) {
@@ -68,13 +74,6 @@ if (require("ggplot2")) {
   library(ggplot2)
 }
 
-if (require("mapproj")) {
-  library(mapproj)
-} else {
-  install.packages("mapproj")
-  library(mapproj)
-}
-
 if (require("viridis")) {
   library(viridis)
 } else {
@@ -82,12 +81,23 @@ if (require("viridis")) {
   library(viridis)
 }
 
+# Vérification si les data sont déjà téléchargé
 if (!file.exists("data.zip")){
+  # Conexion à Kaggle grace à nos identifiant
   kgl_auth(creds_file = 'kaggle.json')
+  # Récupération des data sur la page d l'utilisateur
   response <- kgl_datasets_download_all(owner_dataset = "rohanrao/formula-1-world-championship-1950-2020")
+  # Téléchargement des data
   download.file(response[["url"]], "data.zip", mode="wb")
-  unzip_result <- unzip("data.zip", overwrite = TRUE)
+  # Unzip des data
+  unzip_result <- unzip("data.zip", overwrite = TRUE)       
 }
+
+###########################################################################################################################
+#
+# Partie II : Créatiion des dataframes à partir des fichiers.csv
+#
+###########################################################################################################################
 
 df_circuits <- read.csv('circuits.csv', encoding="UTF-8") 
 df_constructor_results <- read.csv('constructor_results.csv', encoding="UTF-8")
@@ -103,42 +113,39 @@ df_results <- read.csv('results.csv', encoding="UTF-8")
 df_seasons <- read.csv('seasons.csv', encoding="UTF-8")
 df_status <- read.csv('status.csv', encoding="UTF-8")
 
-vector_df <- c(df_circuits,
-               df_constructor_results,
-               df_constructor_standings,
-               df_constructors,
-               df_driver_standings,
-               df_drivers,
-               df_lap_times,
-               df_pit_stops,
-               df_qualifying,
-               df_races,
-               df_results,
-               df_seasons,
-               df_status)
+###########################################################################################################################
+#
+# Partie III : Modification des data et création des fonctions
+#
+###########################################################################################################################
 
+# Récupération de la carte du monde grâce a la fonction map_data() du package ggplot2
 df_world <- map_data("world")
-df_europe <- filter(df_world, region=="Albania" | region=="Finland" | region=="Andorra" | region=="Austria" | region=="Belgium" 
-                    | region=="Bulgaria" | region=="Bosnia and Herzegovina" | region=="Belarus" | region=="Switzerland"
-                    | region=="Czech Republic" | region=="Germany" | region=="Denmark" | region=="Spain" | region=="Estonia"
-                    | region=="France" | region=="UK" | region=="Guernsey" | region=="Greece"
-                    | region=="Croatia" | region=="Hungary" | region=="Isle of Man" | region=="Ireland" | region=="Iceland"
-                    | region=="Italy" | region=="San Marino" | region=="Jersey" | region=="Kosovo" | region=="Liechtenstein"
-                    | region=="Lithuania" | region=="Luxembourg" | region=="Latvia" | region=="Monaco" | region=="Moldova"
-                    | region=="Macedonia" | region=="Montenegro" | region=="Netherlands" | region=="Norway" | region=="Poland"
-                    | region=="Portugal" | region=="Romania" | region=="Serbia" | region=="Slovakia" | region=="Slovenia"
-                    | region=="Sweden" | region=="Ukraine" | region=="Vatican")
+
+# Séparation de chaque pays par continents
+df_europe <- filter(df_world, region=="Albania" | region=="Finland" | region=="Andorra" | region=="Austria" 
+                    | region=="Belgium" | region=="Bulgaria" | region=="Bosnia and Herzegovina" | region=="Belarus" 
+                    | region=="Switzerland" | region=="Czech Republic" | region=="Germany" | region=="Denmark" 
+                    | region=="Spain" | region=="Estonia" | region=="France" | region=="UK" | region=="Guernsey"
+                    | region=="Greece" | region=="Croatia" | region=="Hungary" | region=="Isle of Man" | region=="Ireland" 
+                    | region=="Iceland" | region=="Italy" | region=="San Marino" | region=="Jersey" | region=="Kosovo" 
+                    | region=="Liechtenstein" | region=="Lithuania" | region=="Luxembourg" | region=="Latvia" 
+                    | region=="Monaco" | region=="Moldova" | region=="Macedonia" | region=="Montenegro" 
+                    | region=="Netherlands" | region=="Norway" | region=="Poland" | region=="Portugal" | region=="Romania" 
+                    | region=="Serbia" | region=="Slovakia" | region=="Slovenia" | region=="Sweden" | region=="Ukraine" 
+                    | region=="Vatican")
 
 df_north_america <- filter(df_world, region=="USA" | region=="Mexico" | region=="Canada" | region=="Guatemala" 
                            | region=="Cuba" | region=="Haiti" | region=="Andorra" | region=="Dominican Republic"
-                           | region=="Honduras" | region=="Nicaragua" | region=="El Salvador" | region=="Costa Rica" | region=="Panama"
-                           | region=="Jamaica" | region=="Trinidad and Tobago" | region=="Belize" | region=="Bahamas" 
-                           | region=="Barbados"| region=="Saint Lucia" | region=="Grenada" | region=="Saint Vincent and the Grenadines" 
-                           | region=="Antigua and Barbuda" | region=="Dominica" | region=="Saint Kitts and Nevis")
+                           | region=="Honduras" | region=="Nicaragua" | region=="El Salvador" | region=="Costa Rica" 
+                           | region=="Panama" | region=="Jamaica" | region=="Trinidad and Tobago" | region=="Belize" 
+                           | region=="Bahamas" | region=="Barbados"| region=="Saint Lucia" | region=="Grenada" 
+                           | region=="Saint Vincent and the Grenadines" | region=="Antigua and Barbuda" 
+                           | region=="Dominica" | region=="Saint Kitts and Nevis")
 
 df_south_america <- filter(df_world, region=="Brazil" | region=="Colombia" | region=="Argentina" | region=="Peru" 
-                           | region=="Venezuela" | region=="Chile" | region=="Ecuador" | region=="Bolivia" | region=="Paraguay" 
-                           | region=="Uruguay" | region=="Guyana" | region=="Suriname")
+                           | region=="Venezuela" | region=="Chile" | region=="Ecuador" | region=="Bolivia" 
+                           | region=="Paraguay" | region=="Uruguay" | region=="Guyana" | region=="Suriname")
 
 
 df_asia <- filter(df_world, region=="Russia" | region=="China" | region=="India" | region=="Kazakhstan" 
@@ -156,20 +163,21 @@ df_asia <- filter(df_world, region=="Russia" | region=="China" | region=="India"
 df_africa <- filter(df_world, region=="Nigeria" | region=="Ethiopia" | region=="Democratic Republic of the Congo" 
                     | region=="Egypt" | region=="South Africa" | region=="Tanzania" | region=="Kenya" | region=="Uganda" 
                     | region=="Algeria" | region=="Sudan" | region=="Morocco" | region=="Ghana" | region=="Mozambique" 
-                    | region=="Angola" | region=="Somalia" | region=="Ivory Coast" | region=="Madagascar" | region=="Cameroon" 
-                    | region=="Burkina Faso" | region=="Niger" | region=="Malawi" | region=="Zambia" | region=="Mali" 
-                    | region=="Senegal" | region=="Zimbabwe" | region=="Chad" | region=="Tunisia" | region=="Guinea" 
-                    | region=="Rwanda" | region=="Benin" | region=="Burundi" | region=="South Sudan" | region=="Eritrea" 
-                    | region=="Sierra Leone" | region=="Togo" | region=="Libya" | region=="Central African Republic" 
-                    | region=="Mauritania" | region=="Republic of Congo" | region=="Liberia" | region=="Namibia" 
-                    | region=="Botswana" | region=="Lesotho" | region=="Gambia" | region=="Gabon" | region=="Guinea-Bissau" 
-                    | region=="Mauritius" | region=="Equatorial Guinea" | region=="Eswatini" | region=="Djibouti" 
-                    | region=="Comoros" | region=="Western Sahara" | region=="Cape Verde" | region=="São Tomé and Príncipe" 
-                    | region=="Seychelles")
+                    | region=="Angola" | region=="Somalia" | region=="Ivory Coast" | region=="Madagascar" 
+                    | region=="Cameroon" | region=="Burkina Faso" | region=="Niger" | region=="Malawi" | region=="Zambia" 
+                    | region=="Mali" | region=="Senegal" | region=="Zimbabwe" | region=="Chad" | region=="Tunisia" 
+                    | region=="Guinea" | region=="Rwanda" | region=="Benin" | region=="Burundi" | region=="South Sudan" 
+                    | region=="Eritrea" | region=="Sierra Leone" | region=="Togo" | region=="Libya" 
+                    | region=="Central African Republic" | region=="Mauritania" | region=="Republic of Congo" 
+                    | region=="Liberia" | region=="Namibia" | region=="Botswana" | region=="Lesotho" | region=="Gambia" 
+                    | region=="Gabon" | region=="Guinea-Bissau" | region=="Mauritius" | region=="Equatorial Guinea" 
+                    | region=="Eswatini" | region=="Djibouti" | region=="Comoros" | region=="Western Sahara" 
+                    | region=="Cape Verde" | region=="São Tomé and Príncipe" | region=="Seychelles")
 
 
 df_oceania <- filter(df_world, region=="Australia")
 
+# On rajoute un colonne à df_circuits pour rajouter un coninent par pays d'origine
 df_circuits <- mutate(df_circuits, continent = case_when(country=="UAE" ~ "Asia",
                                                          country=="Japan" ~ "Asia",
                                                          country=="China" ~ "Asia",
@@ -212,79 +220,102 @@ df_circuits <- mutate(df_circuits, continent = case_when(country=="UAE" ~ "Asia"
                                                          
                                                          TRUE ~ "No continent"))
 
+# On définit cette nouvelle colone comme un facteur
 df_circuits[,"continent"] <- factor(df_circuits[,"continent"])
 
 df_circuits <- df_circuits %>%
+  # On compte le nombre de courses sur chaque circuits et on affecte ce nombre à son circuits 
   merge(count(df_races, circuitId), by = "circuitId") %>%
-  arrange(desc(n)) %>%
+  # On arrange dans le sens décroissant pour permettre au plus récent circuits d'être placé en derniers sur la carte
+  arrange(desc(n)) %>% 
+  # On crée une dernière colone contenant le descriptif du circuit
   mutate(text = paste(" Pays: ", country, "\n",
                       "Circuit: ", name, "\n",
                       "Nombre de courses: ", n))
 
+# Création de la foction traçant les carte
 Tracer_carte <- function(data_map, data_circuits, y1, y2, x1, x2, titre) 
 {
   carte <- ggplot() +
+    # On commence par tracer la carte
     geom_polygon(data = data_map, aes(x = long, y = lat, group = group), color = "black", fill = "grey", alpha = 0.3) +
+    # Puis on place les circuit sur la carte
     geom_point(data = data_circuits, aes(x = lng, y = lat, size = n, color = n, text = text, alpha = 1)) +
+    # on définit une échelle de taille pour la représentation des circuits
     scale_size_continuous(range=c(1,10)) +
+    # on définit une échelle de couleur grâce au package viridis
     scale_color_viridis(option="plasma", trans="log2") +
+    # on définit un fond vide et les dimensions du cadre
     theme_void() + ylim(y1, y2) + xlim(x1, x2) + coord_map() +
+    # on définit le titre
     ggtitle(titre)
   
   ggplotly(carte, tooltip="text") 
 }
 
+# On regroupe dans une seule dataframe les Id des coureurs celle et de leur constructeurs pour chaque Id de courses
 df_id <- select(df_results, raceId, driverId, constructorId)
-
-#aggregate(df_constructor_results$points, by = list(df_constructor_results$constructorId), FUN=sum)
-#tapply(df_constructor_results$points, df_constructor_results$constructorId, FUN=sum)
-
+# On affecte à chaque constructeur la somme de ses points pour avoir son score total
 df_results <- df_constructor_results %>% group_by(constructorId) %>% summarise(pointsTotal = sum(points))
 
 df_best_pit_stops <- df_pit_stops %>% 
+  # Rajout de la colone des constructeur dans df_pit_stops
   merge(df_id, by = c("raceId","driverId")) %>%
+  # Création de la colone seconds à partir de la colone miliseconds
   mutate(seconds = milliseconds / 1000) %>%
+  # On ajoute à df_pit_stops les resultats totaux des constructeurs
   merge(df_results, by = "constructorId") %>%
+  # On filtre pour ne garder que les temps cohérent et les meilleurs constructeurs
   filter(pointsTotal >= 1000 & seconds <= 35)
 
+# Création de la foction traçant les histogrammes
 Tracer_histo <- function(data_constructor, color) 
 {
   histo <- ggplot() + 
+    # On trace l'histogramme en foction de l'Id du constructeur choisi.
     geom_histogram(data =  filter(df_best_pit_stops, constructorId == data_constructor), aes(x = seconds), binwidth = 0.5, color = "black", fill = color, alpha = 1) +
+    # on définit le titre
     labs(title = "Durée d'arrêt aux pit stops", x = "Seconde", y = "Occurrence")
   
   ggplotly(histo) 
 }
 
-ui <- dashboardPage(dashboardHeader(title = "Projet F1 DataViz"),
-                    dashboardSidebar(disable = TRUE),
-                    dashboardBody(fluidRow(box(plotlyOutput("circuit", height = 400)),
-                                           box(title = "Régions du monde", selectInput("select_circuit", 
+###########################################################################################################################
+#
+# Partie IV : Création du dashboard Shiny
+#
+###########################################################################################################################
+
+ui <- dashboardPage(dashboardHeader(title = "Projet F1 DataViz"), # Titre
+                    dashboardSidebar(disable = TRUE),  # Pas de Sidebar
+                    dashboardBody(fluidRow(box(plotlyOutput("circuit", height = 400)),  # Premier panel pour la carte prenant l'output "circuit"
+                                           box(title = "Régions du monde", selectInput("select_circuit",   # SelectBox comme input
                                                                                        label = "Régions du monde", 
-                                                                                       choices = c("Monde",
+                                                                                       choices = c("Monde",  # Choix par continent
                                                                                                    "Europe",
                                                                                                    "Amérique du Nord",
                                                                                                    "Amérique du Sud",
                                                                                                    "Asie", 
                                                                                                    "Afrique", 
                                                                                                    "Océanie"), 
-                                                                                       selected = "Monde"))),
-                                  fluidRow(box(plotlyOutput("pit_stop", height = 400)),
-                                           box(title = "Constructor",  selectInput("select_pit_stop", 
+                                                                                       selected = "Monde"))),  # Valeur par default "Monde"
+                                  
+                                  fluidRow(box(plotlyOutput("pit_stop", height = 400)),  # Deuxième panel pour l'a carte'histogramme prenant l'output "pit_stop"
+                                           box(title = "Constructor",  selectInput("select_pit_stop",  # SelectBox comme input
                                                                                    label = "Constructor", 
-                                                                                   choices = c("McLaren",
+                                                                                   choices = c("McLaren",   # Choix par constructur
                                                                                                "Williams",
                                                                                                "Renault",
                                                                                                "Ferrari",
                                                                                                "Red Bull", 
                                                                                                "Force India", 
                                                                                                "Mercedes"), 
-                                                                                   selected = "McLaren"))))
+                                                                                   selected = "McLaren"))))  # Valeur par default "McLaren"
 )
 server <- function(input, output) 
 {
-  output$circuit <- renderPlotly({
-    args <- switch(input$select_circuit,
+  output$circuit <- renderPlotly({  # Création de l'output "circuit"
+    args <- switch(input$select_circuit,  # Fonction générée en fonction de l'input choisi
                    "Monde" = Tracer_carte(df_world, df_circuits, -60, 90, -170, 190, "Carte des circuits de F1 dans le monde"),
                    "Europe" = Tracer_carte(df_europe, filter(df_circuits, continent=="Europe"), 34, 60, -15, 45, "Carte des circuits de F1 en Europe"),
                    "Amérique du Nord" = Tracer_carte(df_north_america, filter(df_circuits, continent=="North America" | name=="Donington Park" | name=="Autodromo Nazionale di Monza"), 0, 100, -170, -50, "Carte des circuits de F1 en Amérique du Nord"),
@@ -294,8 +325,8 @@ server <- function(input, output)
                    "Océanie" = Tracer_carte(df_oceania, filter(df_circuits, continent=="Oceania" | name=="Donington Park" | name=="Autodromo Nazionale di Monza"), -50, -10, 110, 160, "Carte des circuits de F1 en Océanie"))
   })   
   
-  output$pit_stop <- renderPlotly({
-    args <- switch(input$select_pit_stop,
+  output$pit_stop <- renderPlotly({  # Création de l'output "pit_stop"
+    args <- switch(input$select_pit_stop,  # Fonction générée en fonction de l'input choisi
                    "McLaren" = Tracer_histo(1, "orange"),
                    "Williams" = Tracer_histo(3, "blue"),
                    "Renault" = Tracer_histo(4, "yellow"),
@@ -306,4 +337,5 @@ server <- function(input, output)
   })
 }
 
+# création du dashboard
 shinyApp(ui = ui, server = server)
--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# R-Project
+# F1-R-Project
+
+Projet de datavisualisation sur des données de F1 de 1950 à aujourd'hui (La dernière course enregistré est le premier GP d'Autriche de 2021)
+
+## Les objectifs du projet 
+
+Le but de ce projet est de réaliser de la datavisualisation à l'aide du langage R et des librairies fournie par le langage.
+
+## Les prérequis
+
+### D'où vient le dataset ?
+
+Actuellement le projet est un script R dont le dataset est issu d'un projet 'kaggle' sur la F1 dont le lien est fourni ci-dessous :
+
+lien du dataset sur [kaggle](https://www.kaggle.com/rohanrao/formula-1-world-championship-1950-2020).
+
+Le dataset a été mis en ligne par 'Vopani' un utilisateur apparemment régulier de la plateforme ayant le grade de 'Grandmaster'.
+
+Le projet est donc mis assez régulièrement à jour par la communauté. Il est donc possible que des GP de la saison 2021 ou des informations manquantes, voire erronées, soient mises à jour depuis le début du projet.
+
+## User Guide
+
+### L'environement de travail
+
+Nous avons dans ce projet choisi de travailler avec l'environnement [anaconda](https://www.anaconda.com/products/individual), qui propose une grande diversité de libraries prè-installées.
+
+Bien entendu, vous n'êtes pas obligé d'avoir anaconda sur votre machine. Il est néanmoins obligatoire d'avoir R et l'environement Rstudio d'installés sur votre machine pour executer le script. Si tel est le cas vous pouvez passer à l'etape suivante, sinon il vous faudra installer :
+
+- Sous Windows [R](https://cran.r-project.org/bin/windows/base/) et [Rstudio](https://rstudio.com/products/rstudio/download/). 
+
+- Sous linux [R](https://cran.r-project.org/bin/linux/) et [Rstudio](https://rstudio.com/products/rstudio/download/ ).  
+
+Lorsque vous êtes assurés que RSrudio est bien installé dans votre environnement de travail vous n'avez plus qu'a télécharger le projet (conteneant le *F1_dataviz_r.r*, le *kaggle.json*, et le README.md) et à le dézipper. Il vous suffit maintenant de lancer RStudio et et d'aller dans :
+
+- File (dans le coin en haut à gauche)
+  - Open File (troisième ligne)'
+  - Selectioner *F1_dataviz_r.r* dans le dossier précédemment dézippé'
+
+- ou Ctrl+O
+  - Selectioner *F1_dataviz_r.r* dans le dossier précédemment dézippé
+
+Une fois le fichier ouvert cliqué sur *Run App* (en haut au centre). Le fichier vous installera tous les packages et les données s'ils ne sont pas présent sur votre machine et lancera ensuite le dashboard.
+
+## Développer Guide
+
+### Gérer les packages et les données
+
+### Nettoyage des données et les fonctions
+
+### L'exécution du dashboard
+
+## Rapport d'analyse 
+
+Dans ce projet le but était de mettre en évidence des informations à propos de la *F1*. 
+
+Dans un premier temps, nous avons voulu montrer les circuits les plus présents dans le monde de la F1. Nous nous sommes vite rendus compte, grâce à ces graphes, que la F1 est avant tout un sport très européen. La majorité des Grands Prix et des circuits s'y trouve. Et le nombre de course qui ont eux lieu sur chaque circuit est globalement bien plus élevé en Europe que dans le reste du monde.
+
+Dans un second temps, nous avons voulu faire une comparaison sur les temps de pit-stop pour les plus grandes écurie pour pouvoir les départager sur des critères qui ne sont pas uniquement basés sur des données récoltées sur la piste, mais aussi dans les stands. On s'est aperçu d'une sorte d'homogénéité, mais malgré tout, une différence s'est faite sur l'amplitude des résultats (soit le nombre de fois où une écurie arrivé à faire ce temps).
+
+## Conclusion
+
+Le but de ce dashboard est de donner un rapide aperçu de la F1 aux personnes qui ne connaissent pas ou très peu ce milieu, mais aussi de donner des compléments d'informations à des personnes qui connaitraient mieux cette discipline.
+
+Ce projet a vocation à évoluer grâce à des idées de lecture des données supplémentaires ou des idées de personnes intéressées. N'hésitez pas à poster des commentaires et idées constructives pour améliorer le dashboard déjà existant.
